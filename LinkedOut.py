7370ba309f1790460bfba66eefed7ca71016f9ee
Michael Cao <caom1997@uw.edu>
2016-03-23 21:51:08 -0700

    blob

--- LinkedOut.py
+++ LinkedOut.py
@@ -1,6 +1,7 @@
 import os
 import webapp2
 import jinja2
+import random
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -18,10 +19,48 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+class Blod(Handler):
+	pass
+
 class MainPage(Handler):
 	def get(self):
-		self.write()
+		#if self.request.get
+		self.render("randomize.html", blob=randomize(blubs), blub=randomize(blubs), darrenanderson=randomize(blobs))
+
+blubs = ['j my b', 
+	     'j the b', 
+	     'b the b', 
+	     'space invaders', 
+	     'tictactoe', 
+	     'blod', 
+	     'russell wilson', 
+	     'uhhhh', 
+	     'arcade fire', 
+	     'arcade lier', 
+	     'arcade lion', 
+	     'arcade lyer', 
+	     'arcade pyre', 
+	     'i should\'ve arcade inspired'
+	     ]
+
+blobs = ['http://static3.techinsider.io/image/55ba6d1f371d22dd2e8ba492-1106-1012/screen%%20shot%%202015-07-30%%20at%%202.31.57%%20pm.png',
+		 'http://i.imgur.com/mUWtowV.jpg',
+		 'https://img.buzzfeed.com/buzzfeed-static/static/2015-05/11/14/campaign_images/webdr02/1272-rare-pepes-2-32128-1431369756-0_dblbig.jpg',
+		 'https://img.buzzfeed.com/buzzfeed-static/static/2015-10/27/17/enhanced/webdr11/enhanced-mid-29607-1445980104-1.jpg',
+		 'https://img.buzzfeed.com/buzzfeed-static/static/2015-10/27/17/campaign_images/webdr06/what-is-the-market-value-of-a-rare-pepe-2-28727-1445981181-8_big.jpg',
+		 'http://static.fjcdn.com/pictures/Rare+pepe+s+get+yer+rare+pepe+s+here_5a814b_5507279.jpg',
+		 'http://static.fjcdn.com/pictures/Rare_1d0057_5507279.jpg',
+		 'http://static.fjcdn.com/pictures/Rare_2b6c68_5507279.jpg',
+		 'http://static.fjcdn.com/pictures/Rare_a41f3a_5507279.jpg',
+		 'http://static.fjcdn.com/pictures/Rare_7b92c8_5507279.jpg',
+		 'http://static.fjcdn.com/pictures/Rare_26283b_5507279.jpg',
+		 ]
+
+def randomize(blib):
+	temp = random.randrange(0, len(blib))
+	return blib[temp]
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/blod', Blod)
 ], debug=True)
